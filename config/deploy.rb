bd73f5f61f049c98105ce532dc4918d53e964e23
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-02-09 22:16:42 +0300

    dict

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ namespace :deploy do
   desc 'Make sure local git is in sync with remote.'
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/main`
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
         puts 'WARNING: HEAD is not the same as origin/main'
         puts 'Run `git push` to sync changes.'
         exit
