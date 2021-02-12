5bf38e83a62ac26cb23e89dc7bea611887fcacf7
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-02-12 14:04:25 +0300

    podvoh

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import './stylesheets/application'
 import 'bootstrap'
 import firebase from 'firebase/app';
 import "@fortawesome/fontawesome-free/js/all";
